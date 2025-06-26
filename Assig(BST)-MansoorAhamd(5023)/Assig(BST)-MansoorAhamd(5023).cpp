bb85371c376a8323246bd50db4f9778c9f0467d1
Mansoor Ahmad <169179406+MansoorAhmad653@users.noreply.github.com>
2025-06-26 13:13:11 +0500

    Update Assig(BST)-MansoorAhamd(5023).cpp

--- Assig(BST)-MansoorAhamd(5023)/Assig(BST)-MansoorAhamd(5023).cpp
+++ Assig(BST)-MansoorAhamd(5023)/Assig(BST)-MansoorAhamd(5023).cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-struct node
-{
+struct node {
     int c_id;
     string c_name;
     int cre_hrs;
@@ -24,205 +23,200 @@ public:
     Course_info_BST() {
         root = NULL;
     }
-    // Add courses
 
+    // Add a new course
     void Add_course() {
-     
-            node* newnode = new node;
+        node* newnode = new node;
 
-            cout << "Enter Course id: ";
-            cin >> newnode->c_id;
+        cout << "Enter Course ID: ";
+        cin >> newnode->c_id;
 
-            cin.ignore();
-            cout << "Enter Course Name: ";
-            getline(cin, newnode->c_name);
+        cin.ignore();
+        cout << "Enter Course Name: ";
+        getline(cin, newnode->c_name);
 
-            cout << "Enter Credit Hours: ";
-            cin >> newnode->cre_hrs;
-
-
-            if (root == NULL) {
-                root = newnode;
-            }
-            else {
-                node* temp = root;
-
-                while (true) {
-                    if (newnode->c_id < temp->c_id) {
-                        if (temp->left == NULL) {
-                            temp->left = newnode;
-                            break;
-                        }
-                        temp = temp->left;
-                    }
-                    else if (newnode->c_id > temp->c_id) {
-                        if (temp->right == NULL) {
-                            temp->right = newnode;
-                            break;
-                        }
-                        temp = temp->right;
-                    }
-                    else {
-                        cout << "Invalid Entery.";
-                        return;
-                    }
-                }
-            }
-                   
-    }
-    // Add course end
-    
-    //Search course
-    bool Search_course(int search_id) {
-              
+        cout << "Enter Credit Hours: ";
+        cin >> newnode->cre_hrs;
 
         if (root == NULL) {
-            return false;
-        }
-        else {
+            root = newnode;
+        } else {
             node* temp = root;
-
-            while (temp != NULL) {
-                if (search_id < temp->c_id) {
-                  
-                     temp = temp->left;
-                    
-                }
-                else if(search_id == temp->c_id){
-
-                     return true;
-
-                }
-                else if (search_id > temp->c_id) {
-                  
+            while (true) {
+                if (newnode->c_id < temp->c_id) {
+                    if (temp->left == NULL) {
+                        temp->left = newnode;
+                        break;
+                    }
+                    temp = temp->left;
+                } else if (newnode->c_id > temp->c_id) {
+                    if (temp->right == NULL) {
+                        temp->right = newnode;
+                        break;
+                    }
                     temp = temp->right;
-                }
-                else{
-
-                    return false;
+                } else {
+                    cout << "Duplicate Course ID not allowed.\n";
+                    delete newnode;
+                    return;
                 }
             }
         }
     }
-    // Search course end        
-    
-    //Giving Root
+
+    // Search for a course by ID
+    bool Search_course(int search_id) {
+        node* temp = root;
+
+        while (temp != NULL) {
+            if (search_id < temp->c_id) {
+                temp = temp->left;
+            } else if (search_id == temp->c_id) {
+                return true;
+            } else {
+                temp = temp->right;
+            }
+        }
+        return false;
+    }
+
+    // Return the root node
     node* giveroot() {
         return root;
     }
-    //Giving Root end
-    
-    // Delete Course
-    void deleteCourse( int delcour) {
+
+    // Helper function to find minimum value in right subtree
+    node* findMin(node* temp) {
+        while (temp && temp->left != NULL)
+            temp = temp->left;
+        return temp;
+    }
+
+    // Recursive function to delete a node
+    node* deleteNode(node* root, int key) {
         if (root == NULL) {
-            cout << "There is no course it to be deleted." << endl;
-            return;
+            cout << "Course with ID " << key << " not found.\n";
+            return root;
         }
-        else {
 
-            node* temp = root;
-            node* parent = NULL;
-            //while (temp != NULL || temp->c_id == delcour) {
+        if (key < root->c_id) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->c_id) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            // Found the node to delete
+            if (root->left == NULL && root->right == NULL) {
+                delete root;
+                return NULL;
+            } else if (root->left == NULL) {
+                node* temp = root->right;
+                delete root;
+                return temp;
+            } else if (root->right == NULL) {
+                node* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                node* temp = findMin(root->right);
+                root->c_id = temp->c_id;
+                root->c_name = temp->c_name;
+                root->cre_hrs = temp->cre_hrs;
+                root->right = deleteNode(root->right, temp->c_id);
+            }
+        }
+        return root;
+    }
 
-                if (delcour == temp->c_id) {
-                    parent = temp;
-                    cout << "The course is deleted." << endl;
-                    delete temp;
-                }
-            
-        }
+    // Public interface to delete a course
+    void deleteCourse(int id) {
+        root = deleteNode(root, id);
+        cout << "Deletion process complete.\n";
     }
-    // Delete Course end
-    
-    // traversal function
-    void inorder(struct node *root) {
-        node *temp = root;
-        if (temp == NULL) {
-            return;
-        }
-        preorder(temp->left);
-        cout <<"\t Course id: "<< temp->c_id << "\tCourse Name: "<< temp->c_name << "\t Credit Hours: " << temp->cre_hrs << endl;
-        preorder(temp->right);
+
+    // Inorder traversal of BST
+    void inorder(node* root) {
+        if (root == NULL) return;
+
+        inorder(root->left);
+        cout << "Course ID: " << root->c_id
+             << "\tCourse Name: " << root->c_name
+             << "\tCredit Hours: " << root->cre_hrs << endl;
+        inorder(root->right);
     }
-    // traversal function end
 };
-int main()
-{
-    unsigned int choice, course_count = 0;
+
+int main() {
+    unsigned int choice;
     Course_info_BST bst;
+    int course_count = 0;
 
     do {
-        cout << "--------------Menu-------------\n";
-        cout << "1. Add Course data.\n";
-        cout << "2. Search Course.\n";
-        cout << "3. Display.\n";
-        cout << "4. Count Courses.\n";
-        cout << "5. Delete Course.\n";
-        cout << "6. Exit.\n";
+        cout << "\n-------------- Course Management Menu -------------\n";
+        cout << "1. Add Course\n";
+        cout << "2. Search Course\n";
+        cout << "3. Display All Courses\n";
+        cout << "4. Count Courses\n";
+        cout << "5. Delete Course\n";
+        cout << "6. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
         cout << endl;
 
         switch (choice) {
-        case 1:
-            do {
+            case 1:
+                do {
+                    bst.Add_course();
+                    course_count++;
 
-                bst.Add_course();
-                course_count++;
+                    char choic;
+                    cout << "\nDo you want to enter another course? (Y/N): ";
+                    cin >> choic;
+                    cout << endl;
+                    if (!(choic == 'y' || choic == 'Y'))
+                        break;
+                } while (true);
+                break;
 
-                char choic;
-                cout << "\nDo you want to Enter Another Course(Y/N)?";
-                cin >> choic;
-                cout << endl;
-                if (!(choic == 'y' || choic == 'Y')){
-
-                    break;
+            case 2: {
+                int search_id;
+                cout << "Enter Course ID to Search: ";
+                cin >> search_id;
+                if (bst.Search_course(search_id)) {
+                    cout << "\n✔ Course Found with ID: " << search_id << endl;
+                } else {
+                    cout << "\n✘ Course Not Found.\n";
                 }
-              
-            } while (true);
-            break;
-
-        case 2:
-            int search_id;
-
-            cout << "Enter Course id You want to Search: ";
-            cin >> search_id;
-
-            if(bst.Search_course(search_id)){
-
-                cout<<"\n ******************** Course is found with Course id: "<< search_id<<" ******************** " << endl;
-
-                break;
-            }
-            else {
-                cout<<"\n******************** Course is not found. ******************** "<<endl;
                 break;
             }
 
-        case 3:
-            bst.preorder(bst.giveroot());
+            case 3:
+                cout << "All Courses (Inorder Traversal):\n";
+                bst.inorder(bst.giveroot());
+                break;
 
-            break;
+            case 4:
+                cout << "Number of courses: " << course_count << endl;
+                break;
 
-        case 4:
-            cout << "Number of Course are " << course_count << endl;
-            
-            break;
-        case 5:
-            int delete_id;
+            case 5: {
+                int delete_id;
+                cout << "Enter Course ID to Delete: ";
+                cin >> delete_id;
+                bst.deleteCourse(delete_id);
+                course_count--;  // optional: be careful if deletion fails
+                break;
+            }
 
-            cout << "Enter Course id You want to delete: ";
-            cin >> delete_id;
-            bst.deleteCourse(delete_id);
-            break;
-        case 6:
-            exit(-1);
-        default:
-            cout << "Invalid choice! Please try again.\n";
-            break;
+            case 6:
+                cout << "Exiting...\n";
+                exit(0);
+                break;
+
+            default:
+                cout << "Invalid choice. Please try again.\n";
+                break;
         }
     } while (true);
-
 
     return 0;
 }
